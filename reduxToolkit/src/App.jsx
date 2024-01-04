7b94a8adcb53c29ea3a271072e1ebe7c36a40d4b
Mukul Kumar <145434427+dev-mukul@users.noreply.github.com>
2024-01-04 19:31:39 +0000

    redux tool kit

--- reduxToolkit/src/App.jsx
+++ reduxToolkit/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import AddTodo from './components/AddTodo'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
      <h1>Learn about redux toolkit </h1>
+     <AddTodo/>
+     <Todos/>
     </>
   )
 }
